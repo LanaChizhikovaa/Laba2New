cee9fa21decbddf12d0a04c4f1c1f9e5477d9dfa
LanaChizhikovaa <118474139+LanaChizhikovaa@users.noreply.github.com>
2023-05-16 15:18:26 +0700

    Update Observer.h

--- Laba2-master/Project3/Observer.h
+++ Laba2-master/Project3/Observer.h
@@ -9,7 +9,7 @@ using namespace std;
 class Observer: public QObject //интерфейс наблюдателя
     Q_OBJECT
 {
-public:
+public slots:
     virtual void UpdateExist (bool exist) = 0; //обновление существования файла (чистая виртуальная функция)
     virtual void UpdateSize (int size) = 0; //обновление размера файла
 };
