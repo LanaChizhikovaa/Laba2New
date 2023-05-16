c7e5c9ef5baa87b84d142bd878546ad19a17b7af
LanaChizhikovaa <118474139+LanaChizhikovaa@users.noreply.github.com>
2023-05-16 15:11:09 +0700

    Update Subject.h

--- Laba2-master/Project3/Subject.h
+++ Laba2-master/Project3/Subject.h
@@ -2,18 +2,18 @@
 #define SUBJECT_H
 
 #include <vector>
-
+#include <QObject>
 #include "MyFile.h"
 
-class Subject   //класс, отслеживающий наблюдателей
-{
+class Subject: public QObject //класс, отслеживающий наблюдателей
+{         Q_OBJECT
     std::vector<Observer*> files; //список наблюдаемых файлов
 
   public:
     void Attach (Observer*file_); //добавить в отслеживаемые
     void Detach (Observer*file_); //убрать из отслеживаемых
-    void NotifyExist (bool exist_); //изменить существование и уведомить об изменении
-    void NotifySize (int size_); //изменить размер и уведомить об изменении
+    //void NotifyExist (bool exist_); //изменить существование и уведомить об изменении
+    //void NotifySize (int size_); //изменить размер и уведомить об изменении
 };
 
 #endif // SUBJECT_H
