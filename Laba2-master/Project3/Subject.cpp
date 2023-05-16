c69a544fc061bef8b292cd40f131881db6153ef0
LanaChizhikovaa <118474139+LanaChizhikovaa@users.noreply.github.com>
2023-05-16 16:09:33 +0700

    Update Subject.cpp

--- Laba2-master/Project3/Subject.cpp
+++ Laba2-master/Project3/Subject.cpp
@@ -5,29 +5,33 @@
 void Subject::Attach(Observer*file_)
 {
     files.push_back(file_);
+    QObject::connect(this,&Subject::SignalExist,file_,&Observer::UpdateExist); //связываем сигнал со слотом
+    QObject::connect(this,&Subject::SignalSize,file_,&Observer::UpdateSize);
 }
 
 void Subject::Detach(Observer*file_)
 {
    files.erase(remove(files.begin(),files.end(), file_), files.end());
+    disconnect(this,&Subject::SignalExist,file_,&Observer::UpdateExist);
+     disconnect(this,&Subject::SignalSize,file_,&Observer::UpdateSize);
 }
 
-void Subject::NotifyExist(bool exist_)
-{
-    //ищем нужный файл, меняем существование методом MyFile::UpdateExist
-    for (vector<Observer*>::const_iterator iter = files.begin(); iter != files.end(); ++iter) {
-        if (*iter != 0) {
-            (*iter)->UpdateExist(exist_);
-        }
-    }
-}
+// void Subject::NotifyExist(bool exist_)
+// {
+//     //ищем нужный файл, меняем существование методом MyFile::UpdateExist
+//     for (vector<Observer*>::const_iterator iter = files.begin(); iter != files.end(); ++iter) {
+//         if (*iter != 0) {
+//             (*iter)->UpdateExist(exist_);
+//         }
+//     }
+// }
 
-void Subject::NotifySize(int size_)
-{
-    //ищем нужный файл, меняем размер методом MyFile::UpdateSize
-    for (vector<Observer*>::const_iterator iter = files.begin(); iter != files.end(); ++iter) {
-        if (*iter != 0) {
-            (*iter)->UpdateSize(size_);
-        }
-    }
-}
+// void Subject::NotifySize(int size_)
+// {
+//     //ищем нужный файл, меняем размер методом MyFile::UpdateSize
+//     for (vector<Observer*>::const_iterator iter = files.begin(); iter != files.end(); ++iter) {
+//         if (*iter != 0) {
+//             (*iter)->UpdateSize(size_);
+//         }
+//     }
+// }
